a0ce50820581d2c64d1192f7271f03d96e2f1be4
Yash Gosavi <iyashgosavi02@gmail.com>
2024-10-01 11:08:10 +0530

    utility added

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+
+const app = express();
+
+// configuring cors
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials : true
+}))
+
+// limiting JSON
+app.use(express.json({
+    limit:"16kb"
+}))
+
+// URL data configure
+app.use(express.urlencoded({
+    extended:true,
+    limit:"16kb"
+}))
+
+// public assets
+app.use(express.static('public'))
+
+// cookie-parser configure
+app.use(cookieParser())
+
+
+
+
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ console.log(process.env);
 */
 import dotenv from "dotenv";
 import connectDB from './db/index.js';
+import { app } from "./app.js";
 
 dotenv.config({
     path : './env'
 })
 
 connectDB()
+.then(()=>{
+    app.on("ERROR",(error)=>{
+        console.log('ERROR : ',error);
+        process.exit(1);
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server running at port : ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB connection failed!! ", error);
+})
 
 
 
