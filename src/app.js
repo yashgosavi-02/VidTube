d278efc0d69e1e6611882e7ac8f4a4f3d8562f65
Yash Gosavi <iyashgosavi02@gmail.com>
2024-10-15 17:27:13 +0530

    cloudinary_multer

--- src/app.js
+++ src/app.js
@@ -29,7 +29,4 @@ app.use(express.static('public'))
 app.use(cookieParser())
 
 
-
-
-
 export { app };
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath) return null;
+        // upload file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type : "auto"
+        })
+        // successful file upload
+        console.log("File uploaded successfully ", response.url);
+        return response;
+    } catch (error){
+        // removing the locally saved temporary file as upload failed
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
+
+export {uploadOnCloudinary};
